3f06ba322c1ffff46a4bbf0efa79b1451c60dbff
Kishan Kumar <kishan.kumar@nagarro.com>
2024-03-27 23:31:01 +0530

    This is my first commit

---
+++ src/test/java/maven_jekins/maven_jekins/pagetest1.java
@@ -0,0 +1,27 @@
+package maven_jekins.maven_jekins;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class pagetest1 {
+	
+	@Test
+	public void Testgoogle() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("run",Keys.ENTER);
+		System.out.println(driver.getTitle());
+		Thread.sleep(5000);
+		driver.quit();
+	}
+
+}
+
